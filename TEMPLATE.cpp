af2ec4be745f5e764759a425f7634d7ffc0e5043
Paulo Filipe Moreira da Silva <59426206+filipemsilv4@users.noreply.github.com>
2025-04-04 11:24:43 -0300

    Create TEMPLATE.cpp

---
+++ TEMPLATE.cpp
@@ -0,0 +1,19 @@
+/*
+g++ -std=c++17 -o bin/out a.cpp && ./bin/out
+*/
+
+#include <bits/stdc++.h>
+
+#define lli long long int
+#define ulli unsigned long long int
+#define endl "\n"
+
+using namespace std;
+
+int main(){
+    lli t;
+    cin >> t;
+    while (t--){
+
+    }
+}
