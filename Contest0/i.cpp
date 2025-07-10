312004a88d9600247e914fcf82fddf6b03f8a0ab
Paulo Filipe Moreira da Silva <filipemsilva@outlook.com.br>
2025-07-10 00:31:25 -0300

    Update i.cpp

--- Contest0/i.cpp
+++ Contest0/i.cpp
@@ -2,13 +2,18 @@
 g++ -Wall -Werror -std=c++17 -o bin/out i.cpp && ./bin/out
 */
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
 
 #define lli long long int
 #define ulli unsigned long long int
 #define endl "\n"
 
 using namespace std;
+
+vector<lli> p;
 
 int main(){
     ios::sync_with_stdio(false);
@@ -18,15 +23,34 @@ int main(){
     cin >> t;
     while (t--){
         lli n; cin >> n;
-        vector<lli> p(n + 1);
-        for (lli i = 1; i <= n; i++) cin >> p[i];
+        
+        p.clear();
+        p.resize(n);
 
-
+        for (lli i = 0; i < n; i++) cin >> p[i];
+        
         bool found = false;
-        for (lli i = 2; i < n; i++){
-            if (p[i - 1] < p[i] && p[i] > p[i + 1]){
+        for (lli j = 1; j < n - 1; j++){
+            bool found_i = false, found_k = false;
+
+            lli i, k;
+            for (i = j - 1; i >= 0; i--){
+                if (p[i] < p[j]){
+                    found_i = true;
+                    break;
+                }
+            }
+
+            for (k = j + 1; k < n; k++){
+                if (p[k] < p[j]){
+                    found_k = true;
+                    break;
+                }
+            }
+
+            if (found_i && found_k){
                 cout << "YES\n";
-                cout << i-1 << " " << i << " " << i+1 << "\n";
+                cout << i + 1 << " " << j + 1 << " " << k + 1 << "\n";
                 found = true;
                 break;
             }
@@ -35,5 +59,7 @@ int main(){
         if (found) continue;
 
         cout << "NO\n";
+
+        // cout << "------------\n";
     }
 }
